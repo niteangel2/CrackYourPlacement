8f8c3071d646ae673f22f5a1f2ba3badc012f843
Vaishnavi Saxena <111011311+niteangel2@users.noreply.github.com>
2024-09-10 18:48:47 +0530

    Time: 0 ms (100%), Space: 41.8 MB (45.5%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 { // better solution O(2n)
     public void sortColors(int[] nums) {
         int count=0;
         int count1=0;
@@ -19,5 +19,32 @@ class Solution {
         nums[k++]=1;
         for(int i=0;i<count2;i++)
         nums[k++]=2;
+    }
+}
+
+class Solution { // optimal solution dutch national flag algo
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        while(mid <= high) {
+            if(nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                mid++;
+                low++;
+            }
+            else if(nums[mid] == 1) { //already sorted in hypothetical array
+                mid++;
+            }
+            else {//shrinking array
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+
+        }
     }
 }
